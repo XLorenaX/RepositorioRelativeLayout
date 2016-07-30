36b0f9ee329ac5445e63e3b71dd38e84f96962c6
XLorenaX <lorenanascimento.ifpb@gmail.com>
2016-07-30 17:42:53 -0300

    Atribuição da activity geek

--- ExemplosRelativeLayout/app/src/main/java/com/example/manoel/exemplosrelativelayout/MainActivity.java
+++ ExemplosRelativeLayout/app/src/main/java/com/example/manoel/exemplosrelativelayout/MainActivity.java
@@ -2,16 +2,30 @@ package com.example.manoel.exemplosrelativelayout;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
 import com.coderefer.androidsplashscreenexample.R;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton Btn1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton Btn1 = (ImageButton) findViewById(R.id.img44);
+        ImageButton Btn2 = (ImageButton) findViewById(R.id.img10);
+        ImageButton Btn3 = (ImageButton) findViewById(R.id.img12);
 
         Btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gelo = new Intent(MainActivity.this, ArteMordernaActivity.class);
+                startActivity(gelo);
+            }
 
+        });
     }
 }
